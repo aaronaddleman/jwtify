24b6472fdd1eb144213509f98f8446ca4f0e734f
Aaron Addleman <aaron@hiigara.local>
2018-03-25 21:28:52 -0700

    trying to get the repl working...
    * so far i have to install the jsonwebtoken
    
    lein npm install jsonwebtoken

--- src/jwtify/core.cljs
+++ src/jwtify/core.cljs
@@ -1,44 +1,13 @@
 (ns jwtify.core
-  (:require [cljs-lambda.util :as lambda]
-            [cljs-lambda.context :as ctx]
-            [cljs-lambda.macros :refer-macros [deflambda]]
-            [cljs.reader :refer [read-string]]
-            [cljs.nodejs :as nodejs]
-            [cljs.core.async :as async]
-            [promesa.core :as p])
-  (:require-macros [cljs.core.async.macros :refer [go]]))
+  (:require [cljs-lambda.macros :refer-macros [defgateway]]))
 
-(def config
-  (-> (nodejs/require "fs")
-      (.readFileSync "static/config.edn" "UTF-8")
-      read-string))
+(def routes
+  ["/"
+   {"sign"   {:post jwt/sign}
+    "unsign" {:post jwt/unsign}}])
 
-(defmulti cast-async-spell (fn [{spell :spell} ctx] (keyword spell)))
+(defgateway jwtify
+  [& args]
+  {:status 200
+   :body "{\"hello\": \"there\"}"})
 
-(defmethod cast-async-spell :delay-channel
-  [{:keys [msecs] :or {msecs 1000}} ctx]
-  (go
-    (<! (async/timeout msecs))
-    {:waited msecs}))
-
-(defmethod cast-async-spell :delay-promise
-  [{:keys [msecs] :or {msecs 1000}} ctx]
-  (p/promise
-   (fn [resolve]
-     (p/schedule msecs #(resolve {:waited msecs})))))
-
-(defmethod cast-async-spell :delay-fail
-  [{:keys [msecs] :or {msecs 1000}} ctx]
-  (go
-    (<! (async/timeout msecs))
-    ;; We can fail/succeed wherever w/ fail!/succeed! - we can also
-    ;; leave an Error instance on the channel we return, or return a reject
-    ;; promised - see :delayed-failure above.
-    (ctx/fail! ctx (js/Error. (str "Failing after " msecs " milliseconds")))))
-
-(deflambda work-magic [{:keys [magic-word] :as input} ctx]
-  (when (not= magic-word (config :magic-word))
-    (throw (js/Error. "Your magic word is garbage")))
-  (if (= (input :spell) "echo-env")
-    (ctx/environment ctx)
-    (cast-async-spell input ctx)))
